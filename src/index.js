259a2dc2092ec59d12990db265df1e681db2e53d
TetianaMyslynska <tetmyslynska@gmail.com>
2022-05-22 17:38:10 +0200

    task comlete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import './css/styles.css';
+import { refs } from './js/services/getRefs';
+import { fetchCountries } from './js/services/fetchCountries';
+import { clearMarkup } from './js/services/clearMarkup';
+import { renderCountry } from './js/components/countryCard';
+import { renderList } from './js/components/listOfCountries';
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+refs.input.addEventListener('input', debounce(handleCountryInput, DEBOUNCE_DELAY));
+
+function handleCountryInput(e) {
+  const country = e.target.value.trim();
+  if (!country) {
+    clearMarkup(refs);
+    return;
+  }
+  fetchCountries(country)
+    .then(data => {
+      if (data.length > 1 && data.length <= 10) {
+        clearMarkup(refs);
+        renderList(data);
+        return;
+      }
+      if (data.length > 10) {
+        Notify.info('Too many matches found. Please enter a more specific name.');
+        return;
+      }
+      clearMarkup(refs);
+      renderCountry(data);
+    })
+    .catch(err => Notify.failure('Oops, there is no country with that name'));
+}
